588d9f69a0b46f651741bf1f27f745ea517424b7
Raymond <rvaldez216@mycr.redwoods.edu>
2023-01-22 20:27:58 -0800

    Raymond valdez First Assignment Java-Assignment-001

--- src/AlanTuring.java
+++ src/AlanTuring.java
@@ -1,9 +1,21 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Raymond Valdez
+ *
+ * @since Version 1.0
+ *
+ */
 public class AlanTuring {
     /**
      * Prints Alan Turing to the console
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
+    static String name()  {
+        return "Alan Turing";
+    }
     public static void main(String[] args) {
-        // Write your program here
+        AlanTuring.name();
+        System.out.print(AlanTuring.name());
     }
 }
